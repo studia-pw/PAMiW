1003827b9f58312c41498aa7b3f732e1b13d7b4f
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2023-11-27 00:33:00 +0100

    L7 init

---
+++ L7/P09ShopWebAPPMVC.Client/Controllers/CalculatorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P09ShopWebAPPMVC.Client.Controllers
+{
+    //localhost/calculator/
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(IFormCollection form) 
+        {
+            int number1 = int.Parse(form["number1"]);
+            int number2 = int.Parse(form["number2"]);
+
+            int result = number1 + number2;
+
+            ViewBag.Number1 = number1;
+            ViewBag.Number2 = number2;
+            ViewBag.MyResult = result;
+
+            return View("Index");
+            //ViewBag - uzycie dynamicznej wlascwiosci (proste dane)
+            //ViewData - uzycie slownika (proste dane)
+            // przeslanie danych przez model (zlozone dane)
+        }
+    }
+}
---
+++ L7/P12MAUI.Client/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using P04WeatherForecastAPI.Client.ViewModels;
+
+namespace P12MAUI.Client
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage(ProductsViewModel productsViewModel)
+        {
+            
+            InitializeComponent();
+            BindingContext = productsViewModel;
+        }
+
+       
+    }
+}
