94df17608d04c36ff0896389165dce4323c9df7e
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2023-12-02 22:02:55 +0100

    d32a

--- L8/P10ShopWebAPPMVC.Client/Models/OperationModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace P09ShopWebAPPMVC.Client.Models
-{
-    public class OperationModel
-    {
-        public int Number1 { get; set; }
-        public int Number2 { get; set; }
-        public int Result { get; set; }
-    }
-}
